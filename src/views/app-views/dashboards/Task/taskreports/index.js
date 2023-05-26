3a210376a3fcea5138aa5630dacdf53e324a29a5
Meet-Bhalodia <109055372+Meet-Bhalodia@users.noreply.github.com>
2023-05-26 09:13:03 +0530

    912 26-5

--- src/views/app-views/dashboards/Task/taskreports/index.js
+++ src/views/app-views/dashboards/Task/taskreports/index.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React,{useState,useEffect, useRef} from 'react'
+import {CSVLink} from "react-csv"
+import { useReactToPrint } from 'react-to-print';
+import { DeleteOutlined,EditOutlined,FilePdfOutlined} from '@ant-design/icons';
+import {Button} from 'antd'
+import { Table } from "ant-table-extensions";
+import ApiSnippets from 'constants/ApiSnippet'
+const TaskReports = () => {
+  const [dataSource, setDataSource] = useState([])
+  const [data, setData] = useState(null)
+const columns=[
+  {
+  title:'Sr No.',
+  dataIndex:'id',
+  
+  },
+  {
+  title:'Title',
+  dataIndex:'title',
+  },
+  {
+  title:'Description',
+  dataIndex:'description',
+  },
+  {
+  title:'Date',
+  dataIndex:'date',
+  render: (date) => new Date(date * 1000).toLocaleDateString('en-GB'),
+  },
+];
+useEffect(() => {
+  const getData = async () => {
 
-const index = () => {
+    const response = await ApiSnippets("/AdminDashboard",null)
+    setDataSource(response.data.holiday)
+
+  }
+  getData();
+}, [])
+console.log(dataSource);
+const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
   return (
     <div>
-      hello
+     
+            {dataSource && 
+      <>
+    <div ref={componentRef}>
+    <Table
+    // rowSelection={rowSelection}
+     columns={columns}
+     // rowKey={(record) => record.login.uuid}// id
+     dataSource={dataSource}
+    //  pagination={tableParams.pagination}
+    //  loading={loading}
+    //  onChange={handleTableChange}
+    size="small"
+      style={{ overflow: 'auto'}}
+      exportableProps={{ showColumnPicker: true}}
+      searchableProps={{ fuzzySearch: true }}
+   />
+     </div>
+     </>
+   }
     </div>
   )
 }
 
-export default index
+export default TaskReports;
