ee8c89a4f3206d84b5d52959ab269947e1c75d7a
Meet-Bhalodia <109055372+Meet-Bhalodia@users.noreply.github.com>
2023-06-01 18:07:43 +0530

    6 vage 1-6

--- src/views/app-views/dashboards/Task/taskreports/index.js
+++ src/views/app-views/dashboards/Task/taskreports/index.js
@@ -1,58 +1,101 @@
-import React from "react";
-import { DownloadOutlined } from '@ant-design/icons';
-
-import { Button, Card, Divider, Radio, Space, Table, message } from 'antd';
+import React,{useState} from "react";
+import { DownloadOutlined, EditOutlined, MessageOutlined } from "@ant-design/icons";
+import ProductListData from "../../../../../assets/data/dummytaskman.json"
+import { Button, Card, Divider, Radio, Space, Table, message,Menu, Tooltip } from "antd";
 
 // import { Button, Radio } from 'antd';
 // import { DownloadOutlined } from '@ant-design/icons';
-import "./table.css"
+import "./table.css";
 import { useNavigate } from "react-router-dom";
+import EllipsisDropdown from "components/shared-components/EllipsisDropdown";
+import Flex from "components/shared-components/Flex";
+import { EyeOutlined, DeleteOutlined, SearchOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { MdPassword } from "react-icons/md";
 const TaskReports = () => {
-    const Navigate = useNavigate(); 
-    const go = () => {
-        Navigate("/")
-    }
-  return (
-    <Card>
-    <div className="App">
-      <Table border='2'>
-        <thead>
-            <th>srno</th>
-            <th>user name</th>
-            <th>first name</th>
-            <th>last name</th>
-            <th>Email id</th>
-            <th>status</th>
-            <th>action</th>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>yash07</td>
-                <td>yash</td>
-                <td>soni</td>
-                <td>a@a.com</td>
-                <td>active</td>
-                <td>
-                    <Button.Group outerCount='5' >
-                        <Button size='small' type='primary' onClick={go}>edit</Button>
-                        <Button size='small' type='primary' onClick={() => message.warning('delete')}>delete</Button>
-                        <Button size='small' type='primary' onClick={() => message.success('edit')}>edit</Button>
-                        <Button size='small' type='primary' onClick={() => message.warning('delete')}>delete</Button>
-                    </Button.Group>
-                </td>
-            </tr>
-        </tbody>
+	const [list, setList] = useState(ProductListData)
+  const Navigate = useNavigate();
   
-      </Table>
-      
-    </div>
-    
-    <div>
-    
-    </div>
+  const go = () => {
+    Navigate("/");
+  };
+
+  const viewDetails = row => {
+		Navigate(`/app/apps/ecommerce/edit-product/${row.id}`)
+	}
+  
+  
+
+  const tableColumns = [
+		{
+			title: 'ID',
+			dataIndex: 'id'
+		},
+		
+		{
+			title: 'user_name',
+			dataIndex: 'user_name',
+		},
+		{
+			title: 'first_name',
+			dataIndex: 'first_name',
+		},
+		{
+			title: 'last_name',
+			dataIndex: 'last_name',
+		},
+		{
+			title: 'email',
+			dataIndex: 'email',
+		},
+		{
+			title: 'status',
+			dataIndex: 'status',
+		},
+		{
+			title: 'action',
+			dataIndex: '',
+      render: (_, elm) => (
+				
+          <div className="text-right d-flex justify-content-center">
+						<Tooltip title="Edit">
+							<Button success type="primary" className="mr-2" icon={<EditOutlined />} onClick={() => {this.showUserProfile(elm)}} size="small"/>
+						</Tooltip>
+						<Tooltip title="Delete">
+							<Button danger className="mr-2" icon={<DeleteOutlined />} onClick={()=> {this.deleteUser(elm.id)}} size="small"/>
+						</Tooltip>
+						<Tooltip title="Reset Password">
+							<Button danger className="mr-2" icon={<MdPassword />} onClick={()=> {this.deleteUser(elm.id)}} size="small"/>
+						</Tooltip>
+						<Tooltip title="View">
+							<Button danger className="mr-2" icon={<EyeOutlined />} onClick={()=> {this.deleteUser(elm.id)}} size="small"/>
+						</Tooltip>
+						<Tooltip title="Message">
+							<Button danger className="mr-2" icon={<MessageOutlined />} onClick={()=> {this.deleteUser(elm.id)}} size="small"/>
+						</Tooltip>
+					</div>
+				
+			)
+		}
+	];
+
+
+  return (
+    <Card bodyStyle={{'padding': '0px'}} >
+      <div className="">
+      <Table 
+					columns={tableColumns} 
+					dataSource={list} 
+					rowKey='id' 
+				/>
+      </div>
     </Card>
   );
 };
 
 export default TaskReports;
+{/* <Button.Group outerCount='5' >
+    <Button size='small' type='primary' onClick={go}>edit</Button>
+    <Button size='small' type='primary' onClick={() => message.warning('delete')}>delete</Button>
+    <Button size='small' type='primary' onClick={() => message.success('edit')}>edit</Button>
+    <Button size='small' type='primary' onClick={() => message.warning('delete')}>delete</Button>
+</Button.Group> */}
